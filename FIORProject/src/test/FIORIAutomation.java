182098bbf484c4a6be5ee778a8427c1b504ecab8
Abhinesh467 <ajay.tomala@numerictech.com>
2020-02-11 03:19:16 +0530

    test

--- FIORProject/src/test/FIORIAutomation.java
+++ FIORProject/src/test/FIORIAutomation.java
@@ -13,7 +13,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
